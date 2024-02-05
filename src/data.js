f0221a510abd05b4a388246e3978fdc78163c0d6
Germán Freixinós <14282156+germanfrelo@users.noreply.github.com>
2024-02-05 18:25:24 +0100

    Show a basic mockup of elements (#3)

---
+++ src/data.js
@@ -0,0 +1,114 @@
+const elements = [
+	// Películas
+	{
+		id: 1,
+		name: "The Matrix",
+		type: "pelicula",
+		releaseDate: "1999-03-31",
+		createdDate: "2024-02-05",
+	},
+	{
+		id: 2,
+		name: "Inception",
+		type: "pelicula",
+		releaseDate: "2010-07-16",
+		createdDate: "2024-02-05",
+	},
+	{
+		id: 3,
+		name: "The Shawshank Redemption",
+		type: "pelicula",
+		releaseDate: "1994-09-23",
+		createdDate: "2024-02-05",
+	},
+	{
+		id: 4,
+		name: "Pulp Fiction",
+		type: "pelicula",
+		releaseDate: "1994-10-14",
+		createdDate: "2024-02-05",
+	},
+	{
+		id: 5,
+		name: "The Dark Knight",
+		type: "pelicula",
+		releaseDate: "2008-07-18",
+		createdDate: "2024-02-05",
+	},
+
+	// Videojuegos
+	{
+		id: 6,
+		name: "Super Mario Odyssey",
+		type: "videojuego",
+		releaseDate: "2017-10-27",
+		createdDate: "2024-02-05",
+	},
+	{
+		id: 7,
+		name: "The Legend of Zelda: Breath of the Wild",
+		type: "videojuego",
+		releaseDate: "2017-03-03",
+		createdDate: "2024-02-05",
+	},
+	{
+		id: 8,
+		name: "Grand Theft Auto V",
+		type: "videojuego",
+		releaseDate: "2013-09-17",
+		createdDate: "2024-02-05",
+	},
+	{
+		id: 9,
+		name: "Red Dead Redemption 2",
+		type: "videojuego",
+		releaseDate: "2018-10-26",
+		createdDate: "2024-02-05",
+	},
+	{
+		id: 10,
+		name: "The Witcher 3: Wild Hunt",
+		type: "videojuego",
+		releaseDate: "2015-05-19",
+		createdDate: "2024-02-05",
+	},
+
+	// eBooks
+	{
+		id: 11,
+		name: "To Kill a Mockingbird",
+		type: "ebook",
+		releaseDate: "1960-07-11",
+		createdDate: "2024-02-05",
+	},
+	{
+		id: 12,
+		name: "1984",
+		type: "ebook",
+		releaseDate: "1949-06-08",
+		createdDate: "2024-02-05",
+	},
+	{
+		id: 13,
+		name: "The Great Gatsby",
+		type: "ebook",
+		releaseDate: "1925-04-10",
+		createdDate: "2024-02-05",
+	},
+	{
+		id: 14,
+		name: "Brave New World",
+		type: "ebook",
+		releaseDate: "1932-01-01",
+		createdDate: "2024-02-05",
+	},
+	{
+		id: 15,
+		name: "The Catcher in the Rye",
+		type: "ebook",
+		releaseDate: "1951-07-16",
+		createdDate: "2024-02-05",
+	},
+];
+
+export default elements;
