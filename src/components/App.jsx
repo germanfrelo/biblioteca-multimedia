196379bc0b5d04f8802941cf44750448d5dadd12
Germán Freixinós López <14282156+germanfrelo@users.noreply.github.com>
2024-02-06 11:59:37 +0100

    Add logic for searching/filtering elements

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,21 @@ import ElementsList from "./ElementsList";
 import SearchForm from "./SearchForm";
 
 function App() {
+	const [elements, setElements] = useState(initialElements);
 	const [searchTerm, setSearchTerm] = useState("");
+
+	function filterElements(elements, searchTerm) {
+		if (searchTerm === "") {
+			return elements;
+		}
+		const filteredElements = elements.filter(({ name, createdDate }) => {
+			return name.toLowerCase().includes(searchTerm.toLowerCase()) || createdDate.includes(searchTerm);
+		});
+		return filteredElements;
+	}
+
+	const filteredElements = filterElements(elements, searchTerm);
+
 	return (
 		<>
 			<h1>Biblioteca multimedia</h1>
@@ -13,7 +27,15 @@ function App() {
 			<SearchForm searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
 
 			<main>
-				<ElementsList elements={initialElements} />
+				{!filteredElements.length ? (
+					<p>{`No hay resultados para "${searchTerm}"`}</p>
+				) : (
+					<>
+						{searchTerm ? <p>{`${filteredElements.length} resultados para "${searchTerm}"`}</p> : <p>{`${filteredElements.length} resultados`}</p>}
+
+						<ElementsList elements={filteredElements} />
+					</>
+				)}
 			</main>
 		</>
 	);
