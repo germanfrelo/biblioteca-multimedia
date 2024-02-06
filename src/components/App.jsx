671520e5f00e7d150a11bc926503b6a6c19d7597
Germán Freixinós López <14282156+germanfrelo@users.noreply.github.com>
2024-02-06 11:26:46 +0100

    Refactor elements list to its own component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,52 +1,14 @@
-import { elementTypes, initialElements } from "../data";
+import { initialElements } from "../data";
 import "./App.css";
-import MediaElement from "./MediaElement";
+import ElementsList from "./ElementsList";
 
 function App() {
-	const movies = initialElements.filter((element) => element.type === elementTypes.movie);
-	const videoGames = initialElements.filter((element) => element.type === elementTypes.videoGame);
-	const ebooks = initialElements.filter((element) => element.type === elementTypes.eBook);
-
 	return (
 		<>
 			<h1>Biblioteca multimedia</h1>
 
-			<main className="grid">
-				<section>
-					<h2>Películas</h2>
-
-					<ul className="list-unstyled grid">
-						{movies.map(({ id, name, releaseDate, createdDate }) => (
-							<li key={id}>
-								<MediaElement name={name} releaseDate={releaseDate} createdDate={createdDate} />
-							</li>
-						))}
-					</ul>
-				</section>
-
-				<section>
-					<h2>Videojuegos</h2>
-
-					<ul className="list-unstyled grid">
-						{videoGames.map(({ id, name, releaseDate, createdDate }) => (
-							<li key={id}>
-								<MediaElement name={name} releaseDate={releaseDate} createdDate={createdDate} />
-							</li>
-						))}
-					</ul>
-				</section>
-
-				<section>
-					<h2>eBooks</h2>
-
-					<ul className="list-unstyled grid">
-						{ebooks.map(({ id, name, releaseDate, createdDate }) => (
-							<li key={id}>
-								<MediaElement name={name} releaseDate={releaseDate} createdDate={createdDate} />
-							</li>
-						))}
-					</ul>
-				</section>
+			<main>
+				<ElementsList elements={initialElements} />
 			</main>
 		</>
 	);
